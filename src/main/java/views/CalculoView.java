a1900073982b925e04d32e14b00d835fa9411be1
mbaridonn <mbaridonn@gmail.com>
2017-11-04 18:14:54 -0300

    Se muestra el sistema de ecuaciones utilizado en Mostrar Datos

--- src/main/java/views/CalculoView.java
+++ src/main/java/views/CalculoView.java
@@ -8,6 +8,7 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.table.DefaultTableModel;
 
 import controllers.IngresarDatosController;
+import javax.swing.JLabel;
 
 public class CalculoView extends JDialog {
 	
@@ -22,13 +23,35 @@ public class CalculoView extends JDialog {
 		contentPanel.setLayout(null);
 		
 		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(10, 21, 230, 144);
+		scrollPane.setBounds(10, 36, 230, 144);
 		contentPanel.add(scrollPane);
 
 		DefaultTableModel tableModel = ingresarDatosController.getTableModel();
 		
 		JTable table = new JTable(tableModel);
 		scrollPane.setViewportView(table);
+		
+		JLabel lblTablaDeSumatorias = new JLabel("Tabla de sumatorias:");
+		lblTablaDeSumatorias.setBounds(10, 11, 230, 14);
+		contentPanel.add(lblTablaDeSumatorias);
+		
+		JLabel lblSistemaDeEcuaciones = new JLabel("Sistema de ecuaciones:");
+		lblSistemaDeEcuaciones.setBounds(10, 191, 230, 14);
+		contentPanel.add(lblSistemaDeEcuaciones);
+		
+		int cantPuntos = tableModel.getRowCount() - 1;
+		int sum_x = (int) tableModel.getValueAt(cantPuntos, 0);
+		int sum_y = (int) tableModel.getValueAt(cantPuntos, 1);
+		int sum_xCuadrado = (int) tableModel.getValueAt(cantPuntos, 2);
+		int sum_xPorY = (int) tableModel.getValueAt(cantPuntos, 3);
+		
+		JLabel lblSistemaEcuaciones1 = new JLabel("a*"+sum_xCuadrado+" + b*"+sum_x+" = "+sum_xPorY);
+		lblSistemaEcuaciones1.setBounds(10, 216, 230, 14);
+		contentPanel.add(lblSistemaEcuaciones1);
+		
+		JLabel lblSistemaEcuaciones2 = new JLabel("a*"+sum_x+" + b*"+cantPuntos+" = "+sum_y);
+		lblSistemaEcuaciones2.setBounds(10, 241, 230, 14);
+		contentPanel.add(lblSistemaEcuaciones2);
 		
 		ingresarDatosController.setTableModel(tableModel);
 	}
